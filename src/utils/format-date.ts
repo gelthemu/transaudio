c1e1d28b4d510781af6a58d46522490d4ba45c47
gelthemu <gelthemu@gmail.com>
2026-01-08 20:25:19 +0300

    auto

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,18 +1,18 @@
 export const formatDate = (input: number | string): string => {
   const timestamp = typeof input === "string" ? Number(input) : input;
   const date = new Date(timestamp);
-  
+
   if (isNaN(date.getTime())) {
     throw new Error(`Invalid date. Input: ${input}, Parsed: ${timestamp}`);
   }
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short",
     month: "short",
     day: "numeric",
     year: "numeric",
     timeZone: "Africa/Nairobi",
   };
+
   const timeOptions: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
@@ -20,9 +20,7 @@ export const formatDate = (input: number | string): string => {
     timeZone: "Africa/Nairobi",
   };
 
-  const formattedDate = date
-    .toLocaleDateString("en-US", dateOptions)
-    .replace(/(\w+),\s\w+\s(\d+),\s(\d+)/, "$1 $2, $3");
+  const formattedDate = date.toLocaleDateString("en-US", dateOptions);
   const formattedTime = date.toLocaleTimeString("en-US", timeOptions);
 
   return `${formattedDate} at ${formattedTime}`;
